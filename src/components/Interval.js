6f91ce6a8bb5d2d98155b0cba60784aff24ca72c
Micah Lund <micah.r.lund@gmail.com>
2020-08-27 15:27:35 -0600

    Added some comments to explain what I'm doing so when the team looks this over they will understand what the code is doing prior to us having a code review.

--- src/components/Interval.js
+++ src/components/Interval.js
@@ -1,7 +1,13 @@
 import React, {useState, useEffect} from 'react';
+
+//This import is done by using CSS Modules
 import classes from './Component.css';
 
 const Interval = () => {
+
+    //Since this is a functional component, we need to use React hooks in order to manipulate the state.
+    //Below are examples of using useState to set our count, the function that will trigger the countdown, 
+    //whether or not the button was clicked, and change the state of the button click to true or false.
     const [count, startCountdown]= useState(3);
     const [isClicked, setClicked] = useState(false);
 
@@ -9,6 +15,10 @@ const Interval = () => {
         setClicked(true);
     }
 
+    //In order to get the page to re-render, I had to use the useEffect hook. This allowed for me to use the
+    //isClicked to control when my count down started and when I needed it to stop. By using useEffect I was
+    //able to get the countdown to continue after the state initially changed when the page rendered. Without
+    //it the countdown would either only go down by 1 or would just keep going into infinity.
     useEffect(() => {
         let countdown = null;
         if (isClicked && count > 0) {
@@ -25,7 +35,7 @@ const Interval = () => {
         <div className={classes.Interval}>
         <h1>Interval</h1>
         <p>To demonstrate how setInterval works, click the button below. 
-            You should expect to see the number shown countdown from 3 to 0.</p>
+            You should expect to see the number shown count down from 3 to 0.</p>
 
         <button onClick={clicked}>Click me!</button>
         <p className={classes.Message}>{count}</p>
